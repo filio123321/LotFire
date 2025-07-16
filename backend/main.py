bca9b4b17fefb99c18f17bb583eb9d265bb38776
filio <50210737+filio123321@users.noreply.github.com>
2025-07-16 17:38:48 +0300

    Alllow unsafe werkzurg in run

--- backend/main.py
+++ backend/main.py
@@ -180,4 +180,4 @@ def create_app():
 if __name__ == '__main__':
     app, socketio = create_app()
     # use eventlet for concurrency
-    socketio.run(app, host='0.0.0.0', port=8080, debug=True)
+    socketio.run(app, host='0.0.0.0', port=8080, debug=True, allow_unsafe_werkzeug=True)
